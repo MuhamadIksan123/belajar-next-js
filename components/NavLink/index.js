b40ae3beb459fa4deae76b70e7d9b99978323976
Desa Belawa <desabelawa22@gmail.com>
2023-01-02 12:37:40 +0700

    mulai membuat landindpage app

---
+++ components/NavLink/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function NavLink({ href, children }) {
+  // console.log(useRouter());
+  const router = useRouter();
+  return (
+    <div>
+      <Link href={href}>
+        <a
+          className={`nav-link  ${router.pathname === href ? 'active' : ''}`}
+          aria-current="page"
+        >
+          {children}
+        </a>
+      </Link>
+    </div>
+  );
+}
