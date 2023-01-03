a3ab9ac3292797f056f58d3b829c52e816e8ee54
Desa Belawa <desabelawa22@gmail.com>
2023-01-03 13:08:01 +0700

    membuat landingpage part 5

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import NavLink from '../NavLink';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
-import Button from '../Button';
 
 export default function Navbar() {
   const router = useRouter();
@@ -24,10 +23,10 @@ export default function Navbar() {
   return (
     <nav className="container navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
-        <Link href={'/'}>
-          <a className="navbar-brand">
-            <img src="/images/logo.svg" alt="semina" />
-          </a>
+        <Link href={'/'} className="navbar-brand">
+
+          <img src="/images/logo.svg" alt="semina" />
+
         </Link>
         <button
           className="navbar-toggler"
@@ -88,8 +87,8 @@ export default function Navbar() {
                       aria-labelledby="navbarDropdown"
                     >
                       <li>
-                        <Link href={'/dashboard'}>
-                          <a className="dropdown-item">Dashboard</a>
+                        <Link href={'/dashboard'} className="dropdown-item">
+                          Dashboard
                         </Link>
                       </li>
                       <li>
@@ -133,8 +132,8 @@ export default function Navbar() {
                 </div>
               ) : (
                 <div className="d-grid">
-                  <Link href={'/signin'}>
-                    <a className="btn-navy">Sign In</a>
+                  <Link href={'/signin'} className="btn-navy">
+                    Sign In
                   </Link>
                 </div>
               )}
